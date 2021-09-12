2156d515559fdc2b231c55306678c2eb8a5b0bdf
Jeremy Demange <jeremy@scrapfast.io>
2021-09-12 14:20:38 +0200

    Change

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 import streamlit as st
-from transformers import pipeline
-
-classifier = pipeline("text-classification",
-                      model='bhadresh-savani/distilbert-base-uncased-emotion', return_all_scores=True)
 
 st.title('Hello World!')
 
@@ -11,6 +7,5 @@ text = form.text_input(label='Enter some text')
 submit_button = form.form_submit_button(label='Submit')
 
 if submit_button:
-    prediction = classifier(text)
     st.subheader('Data')
-    st.write(prediction)
+    st.write({"text": text})
