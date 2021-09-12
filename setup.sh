e959b4ed2855037435b019532b4116cb5a36ff2f
Jeremy Demange <jeremy@scrapfast.io>
2021-09-12 02:22:53 +0200

    Change setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"\"\n\
-" > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
-port = 80\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
